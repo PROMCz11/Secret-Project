d69c2a3338b4d1ca8174a5db6666705483e67372
Zain Suleiman <109575838+PROMCz11@users.noreply.github.com>
2024-05-10 08:23:03 -0700

    Halved the chances for red boxes to appear

--- script.js
+++ script.js
@@ -8,18 +8,18 @@ animatedGridBoxes.forEach(box => {
     box.classList.toggle('glow');
     const classAssignmentDelay = Math.floor(Math.random() * (max - min + 1)) + min;
     box.style.animationDelay = `${classAssignmentDelay}ms`;
-    const selectedColor = Math.floor(Math.random() * 8) + 1;
+    const selectedColor = Math.floor(Math.random() * 16) + 1;
     setTimeout(() => {
-        if(selectedColor === 1) {
+        if(selectedColor === 1 || selectedColor === 2) {
             box.classList.add('glow-blue');
         }
-        else if(selectedColor === 2) {
+        else if(selectedColor === 3 || selectedColor === 4) {
             box.classList.add('glow-green');
         }
-        else if(selectedColor === 3) {
+        else if(selectedColor === 5 || selectedColor === 6) {
             box.classList.add('glow-orange');
         }
-        else {
+        else if(selectedColor === 7) {
             box.classList.add('glow-red');
         }
     }, classAssignmentDelay);
